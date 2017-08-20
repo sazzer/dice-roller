fb632ab24c51ddf91f0d3f47a04ac49ddd580682
Graham Cox <graham@grahamcox.co.uk>
2017-08-20 17:04:23 +0100

    Actually roll the dice

--- app/models/roll.js
+++ app/models/roll.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     rollName: DS.attr('string'),
     numberOfDice: DS.attr('number'),
-    numberOfSides: DS.attr('number')
+    numberOfSides: DS.attr('number'),
+    result: DS.attr('number')
 });
--- app/routes/roll.js
+++ app/routes/roll.js
@@ -3,11 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     actions: {
         saveRoll: function(rollName, numberOfDice, numberOfSides) {
+            let result = 0;
+            for (let i = 0; i < numberOfDice; ++i) {
+                result += 1 + (parseInt(Math.random() * numberOfSides));
+            }
+
             const store = this.get('store');
             const roll = store.createRecord('roll', {
                 rollName,
                 numberOfDice,
-                numberOfSides
+                numberOfSides,
+                result
             });
             roll.save();
         }
